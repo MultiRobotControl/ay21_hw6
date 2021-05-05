ce0881c8b38eec1a0091e53a3ffc08499eeffe6d
Mccarrin <developer@ip-172-30-49-88.ec2.internal>
2021-05-05 16:17:55 -0700

    Exercise 2 complete

---
+++ cora_echo.m
@@ -0,0 +1,13 @@
+%% 
+% *S.Royster*
+% 
+% *ME4823*
+% 
+% *HW6*
+% 
+% Write a short MATLAB script in your assignment repository ay21_hw6/matlab/cora_echo.m  
+% to echo the message being published by the /cora/sensors/p3d  ROS node.
+% 
+% NOTE: Must run rosinit prior to starting this program
+
+rostopic echo '/cora/sensors/p3d'
